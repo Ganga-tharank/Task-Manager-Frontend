31acae716b2a01e299b29ccd6bc970daf22dd175
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-24 12:57:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Filterbar from './Components/Filterbar';
+import Navbar from './Components/Navbar';
+import { TaskProvider } from './Context/TaskContext';
+import Tasks from "./Components/TaskList"
+
+function App() {
+	return (
+		<>
+			<TaskProvider>
+				<Navbar />
+				<Filterbar />
+				<Tasks />
+			</TaskProvider>
+		</>
+	);
+}
+
+export default App;
